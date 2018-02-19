2224f39542bcc01a55a71e5e5a7f6a45edb00a6b
William Helton <williamlhelton@gmail.com>
2018-02-19 13:37:09 -0500

    add gitignore and flask

---
+++ flask/01-crashcourse/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque earum amet rerum veniam ipsa, dicta numquam commodi eius, quis dolore, deleniti accusamus magni ullam! Exercitationem id atque, voluptatem repudiandae, magnam dolorem illo ducimus accusamus ex, et nam quisquam dignissimos? Impedit blanditiis est consectetur ullam velit. Ullam blanditiis labore molestiae veritatis necessitatibus, ipsa, earum aut commodi voluptas debitis libero dicta expedita voluptate repellat accusamus possimus odio. Temporibus quis, sed molestias saepe dolores magnam itaque fugit alias in natus, placeat facilis sint perspiciatis minima at nam tempore tenetur similique nulla! Modi, aut.',
+            'author': 'William Helton',
+            'create_date': '02-18-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque earum amet rerum veniam ipsa, dicta numquam commodi eius, quis dolore, deleniti accusamus magni ullam! Exercitationem id atque, voluptatem repudiandae, magnam dolorem illo ducimus accusamus ex, et nam quisquam dignissimos? Impedit blanditiis est consectetur ullam velit. Ullam blanditiis labore molestiae veritatis necessitatibus, ipsa, earum aut commodi voluptas debitis libero dicta expedita voluptate repellat accusamus possimus odio. Temporibus quis, sed molestias saepe dolores magnam itaque fugit alias in natus, placeat facilis sint perspiciatis minima at nam tempore tenetur similique nulla! Modi, aut.',
+            'author': 'William Helton',
+            'create_date': '02-19-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque earum amet rerum veniam ipsa, dicta numquam commodi eius, quis dolore, deleniti accusamus magni ullam! Exercitationem id atque, voluptatem repudiandae, magnam dolorem illo ducimus accusamus ex, et nam quisquam dignissimos? Impedit blanditiis est consectetur ullam velit. Ullam blanditiis labore molestiae veritatis necessitatibus, ipsa, earum aut commodi voluptas debitis libero dicta expedita voluptate repellat accusamus possimus odio. Temporibus quis, sed molestias saepe dolores magnam itaque fugit alias in natus, placeat facilis sint perspiciatis minima at nam tempore tenetur similique nulla! Modi, aut.',
+            'author': 'William Helton',
+            'create_date': '02-20-2018'
+        }
+    ]
+    return articles
